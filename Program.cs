7d99af7677bbcd1b2d69e526820944c1276d766b
Chetan Sadvelkar <chetansadvelkar4@gmail.com>
2023-05-16 00:22:55 +0530

    [Chetan] Add: this Script is used to find maximum float number in the array.

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ namespace Generics_Problem
     internal class Program
     {
 
-        public static void printmax(int [] arr)
+        /*public static void printmax(int [] arr)
         {
             Console.WriteLine("Elements in Array: ");
             for (int i = 0; i < arr.Length; i++)
@@ -25,6 +25,28 @@ namespace Generics_Problem
             }
             Console.WriteLine(max);
             Console.WriteLine();
+        }*/
+
+        public static void Printmaxf(float[] arr2 )
+        {
+            Console.WriteLine("Elements in array: ");
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                Console.WriteLine(arr2[i]);
+            }
+            Console.WriteLine();
+
+            float max = arr2[0];
+            Console.WriteLine("Maximum Float in the given array are: ");
+            for(int i = 1; i < arr2.Length; i++)
+            {
+                if (arr2[i] > max)
+                {
+                    max = arr2[i];
+                }
+            }
+            Console.WriteLine(max);
+            Console.WriteLine();    
         }
 
         static void Main(string[] args)
@@ -32,8 +54,14 @@ namespace Generics_Problem
             Console.WriteLine("c# program to Find the Maximum Problem using Generics");
             Console.WriteLine("---------------------------------------------");
 
-            int [] arr = { 5, 2, 9, 8, 3 };
-            printmax(arr);
+            /*int [] arr = { 5, 2, 9, 8, 3 };
+            printmax(arr);*/
+
+            float[] arr2 = { 2.1F, 3.5F, 4.9F };
+            Printmaxf(arr2);
+
         }
+
+        
     }
 }
