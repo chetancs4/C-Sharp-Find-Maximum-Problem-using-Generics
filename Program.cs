99319cc2426ce0d46d2c36675bbfb9e46dbb06a3
Chetan Sadvelkar <chetansadvelkar4@gmail.com>
2023-05-16 00:13:47 +0530

    [Chetan] Add: this Script is used to find maximum integer in the array.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+
+namespace Generics_Problem
+{
+    internal class Program
+    {
+
+        public static void printmax(int [] arr)
+        {
+            Console.WriteLine("Elements in Array: ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            Console.WriteLine();
+
+            int max = arr[0];
+            Console.WriteLine("Maximum integer element in the array are: ");
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+            }
+            Console.WriteLine(max);
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("c# program to Find the Maximum Problem using Generics");
+            Console.WriteLine("---------------------------------------------");
+
+            int [] arr = { 5, 2, 9, 8, 3 };
+            printmax(arr);
+        }
+    }
+}
