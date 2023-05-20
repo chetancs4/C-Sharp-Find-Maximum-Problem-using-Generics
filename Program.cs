e3926031d94e9bfa2194ae69a669382a5754fd48
Chetan Sadvelkar <chetansadvelkar4@gmail.com>
2023-05-20 19:30:33 +0530

    [Chetan] Add: This Script is used to find maximum Using generics.

--- Program.cs
+++ Program.cs
@@ -49,7 +49,7 @@ namespace Generics_Problem
             Console.WriteLine();    
         }*/
 
-        public static void Printmaxs(string[] arr3)
+       /* public static void Printmaxs(string[] arr3)
         {
             Console.WriteLine("Elements in Array: ");
             for (int i = 0; i < arr3.Length; i++)
@@ -67,7 +67,19 @@ namespace Generics_Problem
                 }
             }
             Console.WriteLine("Maximum string in the given array are: "+max);
+        }*/
+
+        //using generics
+        public static T maxOfValues<T> (T a1, T a2, T a3) where T : IComparable<T>
+        {
+            T max = a1;
+            if (a2.CompareTo(max) > 0)
+                max = a2;
+            if (a3.CompareTo(max) > 0)
+                max = a3;
+            return max;
         }
+
 
         static void Main(string[] args)
         {
@@ -80,8 +92,12 @@ namespace Generics_Problem
             /*float[] arr2 = { 2.1F, 3.5F, 4.9F };
             Printmaxf(arr2);*/
 
-            string[] arr3 = { "abcd", "xyz", "pqrstu" };
-            Printmaxs(arr3);
+           /* string[] arr3 = { "abcd", "xyz", "pqrstu" };
+            Printmaxs(arr3);*/
+
+            Console.WriteLine("The maximum value between the three integer is : " + maxOfValues(3, 8, 5) + "\n");
+            Console.WriteLine("The maximum value between the three float is : " + maxOfValues(1.5f, 3.56f, 5.87f) + "\n");
+            Console.WriteLine("The maximum value between the three string is : " + maxOfValues("abc", "pqr", "xyz"));
 
         }
 
